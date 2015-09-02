8537d8690048aef97ca3bdd8ff5fe2728719aa81
Björn Edström <be@bjrn.se>
2015-09-02 19:47:09 +0200

    Rename.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.0.0-DEVEL',
     url='https://github.com/bjornedstrom/python-signify',
     license='MIT',
-    description="Simple wrapper around OpenBSD signify(1) command",
+    description="OpenBSD Signify for Python",
     author=u'Björn Edström',
     author_email='be@bjrn.se',
     packages=['signify'],
